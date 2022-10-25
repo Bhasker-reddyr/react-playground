7e11f6f967acd586e1e47ee3aae4d0ef75109b0b
Bhasker Reddy <cxl-breddy>
2022-10-25 14:04:18 +0530

    Todo example with useReduce

--- src/App.js
+++ src/App.js
@@ -1,21 +1,55 @@
-import React, { useState } from 'react';
+import React, { useReducer, useState } from 'react';
 import './App.css';
+import Todos from './Todos';
+export const ACTIONS = {
+  ADD_TODO: 'add-todo',
+  TOGGLE_TODO: 'toggle-todo',
+  DELETE_TODO: 'delete-todo',
+}
 function App() {
-const [count, setCount] = useState(0)
-function increment(){
-  setCount(prev=> prev+1)
-}
-function decrement(){
-  setCount(prev=> prev-1)
-}
-return (
-    <div className='flex justify-center gap-3 my-10'>
-    <button className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'
-     onClick={increment}>+</button>
-    <span className='py-2'>{count} </span>
-    <button className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'
-     onClick={decrement}>-</button>
-    </div>
+  const [name, setName] = useState('')
+  const [todos, dispatch] = useReducer(reducer, [])
+
+  function reducer(todos, action) {
+    switch (action.type) {
+      case ACTIONS.ADD_TODO:
+        return [...todos, newTodo(action.payload)]
+      case ACTIONS.TOGGLE_TODO:
+        return todos.map((todo) => {
+          if (todo.id === action.payload.id) {
+            return { ...todo, completed: !todo.completed }
+          }
+          return todo
+        })
+      case ACTIONS.DELETE_TODO:
+        return todos.filter(todo => todo.id !== action.payload.id)
+      default:
+        return todos
+    }
+  }
+
+  function newTodo(payload) {
+    return { id: new Date(), name: payload.name, completed: false }
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    dispatch({ type: ACTIONS.ADD_TODO, payload: { name } });
+    setName('')
+  }
+  return (
+    <>
+      <div className='container'>
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={name} onChange={(e) => { setName(e.target.value) }} />
+        </form>
+      </div>
+      <hr />
+      {todos.map((todo => {
+        return <Todos key={todo.id} todo={todo} dispatch={dispatch} />
+      }))
+      }
+    </>
   );
 }
 
---
+++ src/Todos.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Todos.css'
+import {ACTIONS} from './App.js'
+
+function Todos({ todo , dispatch}) {
+    return (
+        <div className='todoWrapper'>
+            <span style={{ color: todo.completed ? "#aaa" : "#000" }}>
+                {todo.name}
+            </span>
+            <button onClick={()=>dispatch({type: ACTIONS.TOGGLE_TODO, payload: {id: todo.id}})}>Toggle</button>
+            <button onClick={()=> dispatch({type:ACTIONS.DELETE_TODO, payload:{id: todo.id}})}>Delete</button>
+        </div>
+    );
+}
+
+export default Todos;
